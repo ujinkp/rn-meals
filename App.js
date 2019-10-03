3b4835f7dc38fc39a1dbd9c76bda09b270742b8b
UjinKP <ujin_kp@meta.ua>
2019-10-03 20:45:35 +0300

    Creating a StackNavigator

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 
+import MealsNavigator from './navigation/MealsNavigator'
+
 const fetchFonts = () => {
   return Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'), 
@@ -17,23 +19,10 @@ export default function App() {
       return (
         <AppLoading 
           startAsync={fetchFonts} 
-          onFinish={() => {setFontLoaded(true)}} 
+          onFinish={() => setFontLoaded(true)} 
          />
       );
   }
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <MealsNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
