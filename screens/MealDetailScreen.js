e41f0bb97c664254c3edccaf2c671710906f7aa4
UjinKP <ujin_kp@meta.ua>
2019-10-05 19:32:05 +0300

    Navigation to the "Meal Details" Screen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {View, Text, StyleSheet  } from 'react-native';
+import {View, Text, Button, StyleSheet  } from 'react-native';
 
 const MealDatailScreen = props => {
     return (
         <View style={styles.screen}>
             <Text>The Meal Datail Screen!</Text>
+            <Button title="Go Back to Categories" onPress={() => {
+                props.navigation.popToTop();
+            }}/>
          </View>
     );
 };
