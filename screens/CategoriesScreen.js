bd9906e55510937ec64f8d3dd59dd0cb50272a53
UjinKP <ujin_kp@meta.ua>
2019-10-06 10:50:15 +0300

    120. Grid Styling & Some Refactoring

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,19 +2,23 @@ import React from 'react';
 import {View, Text, Button, FlatList, StyleSheet, TouchableOpacity, Platform  } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile'
 
 const CategoriesScreen = props => {
     const renderGridItem = (itemData) => {
         return (
-            <TouchableOpacity style={styles.gridItem} onPress={() => {
-                props.navigation.navigate({routeName: 'CategoryMeals', params: {
-                    categoryId: itemData.item.id
-                }});
-            }}>
-                <View>
-                    <Text>{itemData.item.title}</Text>
-                </View>
-            </TouchableOpacity>
+            <CategoryGridTile 
+                title={itemData.item.title} 
+                color={itemData.item.color}
+                onSelect={() => {
+                    props.navigation.navigate({
+                        routeName: 'CategoryMeals', 
+                        params: {
+                            categoryId: itemData.item.id
+                        }
+                    });
+                }}
+            />
         );
     };
     return (
@@ -36,11 +40,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
-    gridItem: {
-        flex: 1,
-        margin: 15,
-        height: 150
-    } 
+     
 }); 
 
 export default CategoriesScreen; 
