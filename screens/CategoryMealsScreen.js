d248d2b6669c59c0943e36fec97fbc761860f651
UjinKP <ujin_kp@meta.ua>
2019-10-06 09:34:22 +0300

    Setting Dynamic Navigation Options

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet  } from 'react-native';
+import {View, Text, Button, StyleSheet, Platform  } from 'react-native';
+
 import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
 const CategoryMealsScreen = props => {
-    const catId = props.navigation.getParam('categoryID')
+    const catId = props.navigation.getParam('categoryId');
 
-    const selecktedCategory = CATEGORIES.find(cat => cat.id === cat.Id)
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
     
     return (
         <View style={styles.screen}>
             <Text>The Category Meals Screen!</Text>
+            <Text>{selectedCategory.title}</Text>
             <Button title="Go to Details" onPress={() => {
                 props.navigation.navigate({
                     routeName: 'MealDetail'
@@ -20,6 +23,20 @@ const CategoryMealsScreen = props => {
             }} />
          </View>
     );
+};
+
+CategoryMealsScreen.navigationOptions = navigationData => {
+    const catId = navigationData.navigation.getParam('categoryId');
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+    return {
+        headerTitle: selectedCategory.title,
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+    };
 };
 
 const styles = StyleSheet.create({
