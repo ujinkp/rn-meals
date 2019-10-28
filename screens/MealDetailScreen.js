89b7174b325918ebfc51e93954a9bb2a39795ea9
UjinKP <ujin_kp@meta.ua>
2019-10-28 20:02:53 +0200

    147. Redux Data & Navigation Options

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { ScrollView, View, Image, Text,  StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'; 
 import { useSelector } from 'react-redux';
@@ -17,9 +17,14 @@ const ListItem = props => {
 const MealDatailScreen = props => {
     const availableMeals = useSelector(state => state.meals.meals);
 
-    const mealId = props.navigation.getParam('mealId');
+    const mealId = props.navigation.getParam ('mealId');
 
     const selectedMeal = availableMeals .find(meal => meal.id === mealId);
+
+    // useEffect(() =>  {
+    //     props.navigation.setParams({mealTitle: selectedMeal.title});
+    // }, [selectedMeal]);
+
 
     return (
         <ScrollView>
@@ -41,9 +46,10 @@ const MealDatailScreen = props => {
 
 MealDatailScreen.navigationOptions = (navigationData) => {
     const mealId = navigationData.navigation.getParam('mealId');
-    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+    const mealTitle = navigationData.navigation.getParam('mealTitle');
+    //const selectedMeal = MEALS.find(meal => meal.id === mealId);
     return {
-        headerTitle: selectedMeal.title,
+        headerTitle: mealTitle, 
         headerRight: 
             <HeaderButtons HeaderButtonComponent={HeaderButton}>
                 <Item 
