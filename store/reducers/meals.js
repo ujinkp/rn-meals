c249e8ea940932ed3a1b69c2c7b4c5001c703301
UjinKP <ujin_kp@meta.ua>
2019-11-05 20:21:53 +0200

    151. Adding Filtering Logic

--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -1,5 +1,5 @@
 import { MEALS } from '../../data/dummy-data';
-import { TOGGLE_FAVORITE } from '../actions/meals';
+import { TOGGLE_FAVORITE, SET_FILTERS } from '../actions/meals';
 
 const initialState = {
     meals: MEALS,
@@ -19,6 +19,24 @@ const mealReducer = (state = initialState, action) => {
                 const meal = state.meals.find(meal => meal.id === action.mealId);
                 return { ...state, favoriteMeals: state.favoriteMeals.concat(meal) }
             }
+        case  SET_FILTERS:
+            const appliedFilters = actions.filters;
+            const updatedFilteredMeals = state.meals.filter(meal => {
+                 if(appliedFilters.glutenFree && !meal.isGlutenFree) {
+                     return false; 
+                 }
+                 if(appliedFilters.lactoseFree && !meal.isLactoseFree) {
+                     return false; 
+                 }
+                 if(appliedFilters.vegetatian && !meal.isVegetarian) {
+                     return false; 
+                 }
+                 if(appliedFilters.vegan && !meal.isVegan ) {
+                     return false; 
+                 } 
+                 return true;
+            });
+            return {...state, filteredMeals: updatedFilteredMeals}
         default:
             return state;
      }
